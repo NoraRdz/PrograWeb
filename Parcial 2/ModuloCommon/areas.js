fbcd3080e146ca606e316fae61ecb36829051fba
NoraRdz <narl031003@gmail.com>
2023-10-02 23:09:15 -0500

    Subir tarea

--- Parcial 2/ModuloCommon/areas.js
+++ Parcial 2/ModuloCommon/areas.js
@@ -1,14 +1,32 @@
 /*config de la extension>settings.json*/
 
-function areaCuadrado(lado){
+    function areaCuadrado(lado){
     return lado*lado;
     }
     
     function areaTriangulo(base,altura){
         return(base*altura/2);
     }
+
+    function areaRectangulo(base,altura){
+        return(base*altura);
+    }
+
+    function elevadoAlCuadrado(n){
+        return(n*n);
+    }
+
+    function areaCirculo(r){
+        return(3.1416*(r*r))
+    }
+
     module.exports.areaCuadrado=areaCuadrado;
     module.exports.areaTriangulo=areaTriangulo;
+    module.exports.areaRectangulo=areaRectangulo;
+    module.exports.elevadoAlCuadrado=elevadoAlCuadrado;
+    module.exports.areaCirculo=areaCirculo;
+
+
    /*1  console.log(__dirname);
     console.log(__filename)*/
     console.log(module) ;
---
+++ Parcial 2/ModuloCommon/calcularareas.js
@@ -0,0 +1,15 @@
+let areas=require("./areas.js");
+let a=areas.areaCuadrado(4);
+console.log(a);
+
+let t=areas.areaTriangulo(3,6);
+console.log(t);
+
+let Cu=areas.areaCuadrado(2,5);
+console.log(Cu);
+
+let e=areas.elevadoAlCuadrado(5);
+console.log(e);
+
+let Ci=areas.areaCirculo(2);
+console.log(Ci);
